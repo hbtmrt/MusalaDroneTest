5726a06a176a1259f79fc2fec3fb94628e8279b7
Haritha Tennakoon <hbt.mrt@gmail.com>
2022-01-13 22:30:58 +0530

    features/musala-01: Added basic project structure for the Web APIs.

---
+++ MusalaDrones/MusalaDrones.WebApi/Controllers/DroneOperatorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusalaDrones.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DroneOperatorController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Test works.";
+        }
+    }
+}
