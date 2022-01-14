d3d1d3cb9ae160e931d6a7be02266704e74bdb2b
Haritha Tennakoon <hbt.mrt@gmail.com>
2022-01-14 12:53:59 +0530

    Musala-01: Added Data layer, dbcontext and seed method.

---
+++ MusalaDrones/MusalaDrones.Data/DbContexts/DroneContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusalaDrones.Core.Models;
+
+namespace MusalaDrones.Data.DbContexts
+{
+    public class DroneContext : DbContext
+    {
+        public DroneContext(DbContextOptions<DroneContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Drone> Drones { get; set; }
+        public DbSet<MedicationItem> MedicationItem { get; set; }
+    }
+}
--- MusalaDrones/MusalaDrones.WebApi/Startup.cs
+++ MusalaDrones/MusalaDrones.WebApi/Startup.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MusalaDrones.Data.DbContexts;
+using MusalaDrones.Data.Seeders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +28,7 @@ namespace MusalaDrones.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DroneContext>(opt => opt.UseInMemoryDatabase(databaseName: "MusalaDrones"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +47,15 @@ namespace MusalaDrones.WebApi
             {
                 endpoints.MapControllers();
             });
+
+            Seed(app);
+        }
+
+        private static void Seed(IApplicationBuilder app)
+        {
+            var serviceScope = app.ApplicationServices.CreateScope();
+            var context = serviceScope.ServiceProvider.GetService<DroneContext>();
+            DroneSeeder.Seed(context);
         }
     }
 }
